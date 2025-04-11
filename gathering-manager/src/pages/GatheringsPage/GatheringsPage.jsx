1b20058d4782f7b00266ba2df5825ed561526431
Kaleb-Scott <kscott47@my.apsu.edu>
2025-04-11 09:49:06 -0500

    hot fix

--- gathering-manager/src/pages/GatheringsPage/GatheringsPage.jsx
+++ gathering-manager/src/pages/GatheringsPage/GatheringsPage.jsx
@@ -1,5 +1,5 @@
 import Header from "../../components/Header/Header";
-import {styles} from "./GatheringsPage.module.css";
+import styles from "./GatheringsPage.module.css";
 
 
 function GatheringsPage() {
