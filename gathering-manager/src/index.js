d0fd47b0ec5c7a47df657b38dab7073c33c025f2
bjohnson130 <bjohnson130@my.apsu.edu>
2025-04-21 09:39:19 -0500

    Update index.js

--- gathering-manager/src/index.js
+++ gathering-manager/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const clientId = "47332756091-qb562fndm3tnj6de2fa1mo24juqot1ac.apps.googleusercontent.com";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
